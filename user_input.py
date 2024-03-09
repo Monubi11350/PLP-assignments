1a868d45c932a1f99aa656e33ae6ad973065e6bf
Monubi11350 <ogenchejustin@gmail.com>
2024-03-09 17:34:36 +0300

    Python basics in taking user input

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name= input('What is your name?')
+age= input('What is your age?')
+location= input('Where are you from?')
+print('Hello {}, you are {} years of age and you come from {}.'.format(name, age, location))
